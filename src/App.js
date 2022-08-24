dcc1ae9ecfd29ba76ad4c5a97854e2288f142e92
Rohit Gupta <lucidrohit@gmail.com>
2022-08-24 21:56:52 +0530

    rechanges in initial styles.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <pre>
-        <h1 style={{color:"red",display:"inline-block", fontSize:"6rem"}}>Zoxtro</h1> is live.
+        <h1 style={{color:"red",display:"inline-block"}}>Zoxtro</h1> is live.
       </pre>
       </header>
     </div>
