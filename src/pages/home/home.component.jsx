22065c8dc6f4926d5e0bd4631e93f0e9e20db49c
Rohit Gupta <lucidrohit@gmail.com>
2022-08-28 16:30:19 +0530

    i think this should be the final home design

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,5 +1,7 @@
 import "./home.styles.scss"
 import ctaLogo from "../../assets/cta.svg"
+import product from "../../assets/product.svg"
+
 const Home = () => {
     return (
         <div className="home" style={{ backgroundImage: `url(${ctaLogo})` }} >
@@ -14,23 +16,42 @@ const Home = () => {
                     <div>
                         <p className="home__cta_content__tagline">
                             Combining the best embroidery designs with the best quality fabric out there to give you the experience that you deserve.
-                                <button className="home__cta_content__tagline__reg_btn">
-                                    Pre-register
-                                </button>
+                            <button className="home__cta_content__tagline__reg_btn">
+                                Pre-register
+                            </button>
 
                         </p>
                     </div>
 
                 </div>
                 <div className="home__cta_product">
+                    <img src={product} alt="Product" className="home__cta_product__image" />
                 </div>
             </div>
             <div className="home__foot">
                 <ul>
                     <li></li>
-                    <li><a href="https://www.facebook.com/Zoxtro-108315835339179/">Fb</a></li>
-                    <li><a href="https://twitter.com/zoxtro">Tw</a></li>
-                    <li><a href="https://www.instagram.com/zoxtro/">Ig</a></li>
+                    <li>
+                        <a href="https://www.facebook.com/Zoxtro-108315835339179/"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Fb
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/zoxtro"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Tw
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/zoxtro/"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Ig
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
