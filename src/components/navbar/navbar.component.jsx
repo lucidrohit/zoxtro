e49375eb4a09e9b760022b4cb96b9918dc0b195b
Rohit Gupta <lucidrohit@gmail.com>
2022-09-01 13:32:13 +0530

    removing scrolling animation scrolling bheaviour to auto in index file

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,46 +1,35 @@
 import "./navbar.styles.scss"
 import Logo from "../../assets/logo.svg"
 import { useState } from "react"
+import Hamburger from "./hamburger.component";
 
-const NavBar = ({isActive}) => {
 
-    window.history.pushState({}, undefined, (isActive?"#home":"#contact"));
+const NavBar = ({ isActive }) => {
 
-    const [contact, setContact] = useState(false)
+    window.history.pushState({}, undefined, (isActive ? "#home" : "#contact"))
+
     const [activeHam, setActiveHame] = useState(false)
 
 
-    const handleHam = () => {
-        console.log(contact);
-        setActiveHame(!activeHam)
-    }
-
     return (
-
-        <>
-            <div className="navbar">
-                <div className="navbar__logo">
-                    <a href="/" >
-                        <img src={Logo} alt="Logo" />
-                    </a>
-                </div>
-                <nav className="navbar__nav_section">
-                    <ul className={activeHam ? " is-active" : " "}>
-                        <li id="list-home" className={isActive ? " active" : " "}>
-                            <a href="#home" onClick={() => setContact(false)}>About{'\u00A0'}</a>
-                        </li>
-                        <li id="list-contact" className={!isActive ? " active" : " "}>
-                            <a href="#contact" onClick={() => setContact(true)}>Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className={`hamburger hamburger--spin ${activeHam ? " is-active" : " "}`} type="button" onClick={() => handleHam()}>
-                    <span className="hamburger-box">
-                        <span className="hamburger-inner"></span>
-                    </span>
-                </div>
+        <div className="navbar">
+            <div className="navbar__logo">
+                <a href="/" >
+                    <img src={Logo} alt="Logo" />
+                </a>
             </div>
-        </>
+            <nav className="navbar__nav_section">
+                <ul className={activeHam ? " is-active" : " "}>
+                    <li id="list-home" className={isActive ? " active" : " "} >
+                        <a  href="#home">About{'\u00A0'}</a>
+                    </li>
+                    <li id="list-contact" className={!isActive ? " active" : " "} >
+                        <a  href="#contact" >Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <Hamburger activeHam={activeHam}  setActiveHame={setActiveHame}/>
+        </div>
     )
 }
 
